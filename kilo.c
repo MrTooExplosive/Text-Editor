592f8c79ff59769b72972f8d22aaa56d1089ac76
Makarius Salib <misalib@cpp.edu>
2021-10-22 09:25:12 +0100

    Adds simple function descriptions and functions for reading and processing a key
    1. Adds a single line comment above each function other than main describing it
    2. Adds editorReadKey() to read each key
    3. Adds editorProcessKeypress() to react to each key press
    4. Simplifies main by replacing body of the while loop with just
       editorProcessKeypress()

--- kilo.c
+++ kilo.c
@@ -10,6 +10,8 @@
 void enableRawMode();
 void disableRawMode();
 void die(const char *s);
+void editorProcessKeyPress();
+char editorReadKey();
 
 struct termios orig_termios;
 
@@ -19,31 +21,50 @@ int main()
 
 	char c;
 	while (1)
-	{
-		c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if (iscntrl(c))
-			printf("%d\r\n", c);
-		else
-			printf("%d ('%c')\r\n", c, c);
-		if (c == CTRL_KEY('q')) break;
-	}
-
+		editorProcessKeypress();
 	return 0;
 }
 
+// Reads a key from the terminal
+char editorReadKey()
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+}
+
+// Process each key read
+void editorProcessKeypress()
+{
+	char c = editorReadKey();
+	switch (c)
+	{
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
+// Prints what the error was and terminates
 void die(const char *s)
 {
 	perror(s);
 	exit(1);
 }
 
+// Returns the terminal to normal mode
 void disableRawMode()
 {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) ==  -1)
 		die("tcsetattr");
 }
 
+// Enables raw mode for character input
 void enableRawMode()
 {
 	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
