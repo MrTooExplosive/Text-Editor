c71fd81e8e4de677c4b0b61bc60b433ef8e19362
Makarius Salib <misalib@cpp.edu>
2021-12-08 03:01:33 +0000

    Adds a fallback method to find window size

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@ void editorRefreshScreen();
 void editorDrawRows();
 int getWindowSize(int *rows, int *cols);
 void initEditor();
+int getCursorPosition(int *rows, int *cols);
 
 struct editorConfig
 {
@@ -38,6 +39,24 @@ int main()
 	return 0;
 }
 
+// Stores the cursor position
+int getCursorPosition(int *rows, int *cols)
+{
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+	printf("\r\n");
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1)
+	{
+		if (iscntrl(c))
+			printf("%d\r\n", c);
+		else
+			printf("%d ('%c')\r\n", c, c);
+	}
+	editorReadKey();
+	return -1;
+}
+
 // Initializes the editor
 void initEditor()
 {
@@ -51,7 +70,9 @@ int getWindowSize(int *rows, int *cols)
 	struct winsize ws;
 	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
 	{
-		return -1;
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999C", 12) != 12)
+			return -1;
+		return getCursorPosition(rows, cols);
 	}
 	else
 	{
