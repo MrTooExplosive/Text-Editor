3b3d6c6a9b2746626d1cc33548c93cbb1a4a9ee2
Makarius Salib <misalib@cpp.edu>
2021-12-10 05:21:54 +0000

    Adds error checking to the saving process

--- kilo.c
+++ kilo.c
@@ -64,6 +64,7 @@ struct abuf
 	int len;
 };
 
+void editorSetStatusMessage(const char *fmt, ...);
 void editorSave();
 char *editorRowsToString(int *buflen);
 void editorRowInsertChar(erow *row, int at, int c);
@@ -97,7 +98,7 @@ int main(int argc, char *argv[])
 	if (argc >= 2)
 		editorOpen(argv[1]);
 
-	editorSetStatusMessage("HELP: Ctrl-Q = quit");
+	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 	while (1)
 	{
 		editorRefreshScreen();
@@ -112,12 +113,23 @@ void editorSave()
 		return;
 	int len;
 	char *buf = editorRowsToString(&len);
-
 	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-	ftruncate(fd, len);
-	write(fd, buf, len);
-	close(fd);
+	if (fd != -1)
+	{
+		if (ftruncate(fd, len) != -1)
+		{
+			if (write(fd, buf, len) == len)
+			{
+				close(fd);
+				free(buf);
+				editorSetStatusMessage("%d bytes written to disk", len);
+				return;
+			}
+		}
+		close(fd);
+	}
 	free(buf);
+	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 char *editorRowsToString(int *buflen)
