9a6fa990e928000153e24598a44ea158df633e80
Makarius Salib <misalib@cpp.edu>
2021-12-10 18:23:18 +0000

    Highlights searched terms

--- kilo.c
+++ kilo.c
@@ -47,7 +47,8 @@ enum editorKey
 enum editorHighlight
 {
 	HL_NORMAL = 0,
-	HL_NUMBER
+	HL_NUMBER,
+	HL_MATCH
 };
 
 struct editorConfig
@@ -133,6 +134,7 @@ int editorSyntaxToColor(int hl)
 	switch (hl)
 	{
 		case HL_NUMBER: return 31;
+		case HL_MATCH: return 34;
 		default: return 37;
 	}
 }
@@ -219,6 +221,7 @@ void editorFindCallback(char *query, int key)
 			E.cy = current;
 			E.cx = editorRowRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
+			memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
 			break;
 		}
 	}
