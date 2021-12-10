b5e41ec7f78e57f92c93295d80a1680c97c4b597
Makarius Salib <misalib@cpp.edu>
2021-12-10 05:25:38 +0000

    Adds a flag to know if the file has been modified since last saving

--- kilo.c
+++ kilo.c
@@ -55,6 +55,7 @@ struct editorConfig
 	char *filename;
 	char statusmsg[80];
 	time_t statusmsg_time;
+	int dirty;
 };
 struct editorConfig E;
 
@@ -122,6 +123,7 @@ void editorSave()
 			{
 				close(fd);
 				free(buf);
+				E.dirty = 0;
 				editorSetStatusMessage("%d bytes written to disk", len);
 				return;
 			}
@@ -168,6 +170,7 @@ void editorRowInsertChar(erow *row, int at, int c)
 	row->size++;
 	row->chars[at] = c;
 	editorUpdateRow(row);
+	E.dirty++;
 }
 
 void editorDrawMessageBar(struct abuf *ab)
@@ -194,7 +197,7 @@ void editorDrawStatusBar(struct abuf *ab)
 {
 	abAppend(ab, "\x1b[7m", 4);
 	char status[80], rstatus[80];
-	int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
 	int rlen = snprintf(rstatus, sizeof(rstatus), "%d%d", E.cy + 1, E.numrows);
 	if (len > E.screencols)
 		len = E.screencols;
@@ -283,6 +286,7 @@ void editorAppendRow(char *s, size_t len)
 	E.row[at].render = NULL;
 	editorUpdateRow(&E.row[at]);
 	E.numrows++;
+	E.dirty++;
 }
 
 // Opens the editor for reading and writing to a file
@@ -307,6 +311,7 @@ void editorOpen(char *filename)
 	}
 	free(line);
 	fclose(fp);
+	E.dirty = 0;
 }
 
 // Moves the cursor
@@ -398,6 +403,7 @@ void initEditor()
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.rx = 0;
+	E.dirty = 0;
 	E.filename = NULL;
 	E.statusmsg[0] = '\0';
 	E.statusmsg_time = 0;
