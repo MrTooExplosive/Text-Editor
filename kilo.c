673ad57dc2474ba4bcf19eb1052b260ac21d95b5
Makarius Salib <misalib@cpp.edu>
2021-12-10 06:13:46 +0000

    Allows saving to a new file name

--- kilo.c
+++ kilo.c
@@ -98,6 +98,7 @@ void editorFreeRow(erow *row);
 void editorDelRow(int at);
 void editorRowAppendString(erow *row, char *s, size_t len);
 void editorInsertNewline();
+char *editorPrompt(char *prompt);
 
 int main(int argc, char *argv[])
 {
@@ -113,6 +114,38 @@ int main(int argc, char *argv[])
 		editorProcessKeypress();
 	}
 	return 0;
+}
+
+char *editorPrompt(char *prompt)
+{
+	size_t bufsize = 128;
+	char *buf = malloc(bufsize);
+	size_t buflen = 0;
+	buf[0] = '\0';
+	while (1)
+	{
+		editorSetStatusMessage(prompt, buf);
+		editorRefreshScreen();
+		int c= editorReadKey();
+		if (c == '\r')
+		{
+			if (buflen != 0)
+			{
+				editorSetStatusMessage("");
+				return buf;
+			}
+		}
+		else if (!iscntrl(c) && c < 128)
+		{
+			if (buflen == bufsize - 1)
+			{
+				bufsize *= 2;
+				buf = realloc(buf, bufsize);
+			}
+			buf[buflen++] = c;
+			buf[buflen] = '\0';
+		}
+	}
 }
 
 void editorInsertNewline()
@@ -192,7 +225,7 @@ void editorRowDelChar(erow *row, int at)
 void editorSave()
 {
 	if (E.filename == NULL)
-		return;
+		E.filename = editorPrompt("Save as: %s");
 	int len;
 	char *buf = editorRowsToString(&len);
 	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
